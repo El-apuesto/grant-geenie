0534db73d2ce987f4336b25607b1617843d79087
El-apuesto <thecitieschoice@gmail.com>
2026-01-08 11:59:00 -0600

    Add fiscal sponsors sample data

---
+++ supabase/migrations/add_fiscal_sponsors_data.sql
@@ -0,0 +1,17 @@
+-- Insert sample fiscal sponsors
+INSERT INTO fiscal_sponsors (name, location, city, state, focus_areas, description, is_active) VALUES
+('Fractured Atlas', 'New York, NY', 'New York', 'NY', ARRAY['Arts', 'Culture'], 'Largest arts fiscal sponsor', true),
+('Tides Center', 'San Francisco, CA', 'San Francisco', 'CA', ARRAY['Social Justice', 'Environment'], 'Major fiscal sponsor', true),
+('Community Partners', 'Los Angeles, CA', 'Los Angeles', 'CA', ARRAY['Nonprofit Support'], 'Major fiscal sponsor', true),
+('Social Good Fund', 'San Francisco, CA', 'San Francisco', 'CA', ARRAY['All Fields'], 'Fast approval for first-timers', true),
+('NYFA', 'New York, NY', 'New York', 'NY', ARRAY['Arts'], 'NY artist support', true),
+('The Field', 'New York, NY', 'New York', 'NY', ARRAY['Performing Arts', 'Dance'], 'Performing arts support', true),
+('Film Independent', 'Los Angeles, CA', 'Los Angeles', 'CA', ARRAY['Film'], 'Independent film LA', true),
+('IDA', 'Los Angeles, CA', 'Los Angeles', 'CA', ARRAY['Film', 'Documentary'], 'Documentary filmmakers', true),
+('Players Philanthropy Fund', 'Towson, MD', 'Towson', 'MD', ARRAY['Sports', 'Youth', 'Health'], 'Athlete-founded philanthropy', true),
+('Inquiring Systems', 'Sonoma, CA', 'Sonoma', 'CA', ARRAY['Education', 'Research', 'Tech'], 'STEM education and research', true),
+('Community Initiatives', 'Oakland, CA', 'Oakland', 'CA', ARRAY['Social Justice', 'Community Development'], 'Grassroots organizing', true),
+('Intersection for the Arts', 'San Francisco, CA', 'San Francisco', 'CA', ARRAY['Arts', 'Social Justice', 'BIPOC'], 'BIPOC artists', true),
+('Springboard for the Arts', 'St. Paul, MN', 'St. Paul', 'MN', ARRAY['Arts'], 'Artist support', true),
+('Shunpike', 'Seattle, WA', 'Seattle', 'WA', ARRAY['Arts'], 'Seattle arts fiscal sponsor', true),
+('NEO Philanthropy', 'New York, NY', 'New York', 'NY', ARRAY['Philanthropy', 'Social Justice'], 'Progressive philanthropy', true);
